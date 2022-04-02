a0c4c142ba6119e88a376404eb6f351341ed612c
Micah Eckard <lumberjacksoul339@gmail.com>
2022-04-02 17:14:22 -0400

    javascript for changing color

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var hoverOnBanner = document.getElementById('banner-h1')
 
-var randomColorOne = Math.floor(Math.random().toFixed(2)*100)
-var randomColorTwo = Math.floor(Math.random().toFixed(2)*100)
-var randomColorThree = Math.floor(Math.random().toFixed(2)*100)
-var randomRgb = 'rgb'+'('+ randomColorOne +','+ randomColorTwo +','+ randomColorThree+ ')'
+//var randomColorOne = Math.floor(Math.random().toFixed(2)*100)
+//var randomColorTwo = Math.floor(Math.random().toFixed(2)*100)
+//var randomColorThree = Math.floor(Math.random().toFixed(2)*100)
+//var randomRgb = 'rgb'+'('+ randomColorOne +','+ randomColorTwo +','+ randomColorThree+ ')'
+function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+  } 
 
-hoverOnBanner.addEventListener("mouseover", function (event) {
-    event.target.style.color =  randomRgb
-})
 
+
+const colorFun = (event) => {
+    event.target.style.color =  randomColor()
+}
+const reset = (event) => {
+    event.target.style.color = ""
+}
+
+hoverOnBanner.addEventListener("mouseover", colorFun)
+hoverOnBanner.addEventListener("mouseout", reset)
